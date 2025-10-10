796ad036301b9ccae7ed30bd0731d3b2146888d0
stanley :) <88297063+stanleyylin@users.noreply.github.com>
2025-10-09 21:58:36 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (8.29%) - LeetSync

---
+++ Arrays/70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # 1d dp: dp[i] = # of ways to climb to the ith step
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0 for _ in range(n+1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
