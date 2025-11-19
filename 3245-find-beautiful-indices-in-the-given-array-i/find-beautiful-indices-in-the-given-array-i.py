d0a8749179952cc20861825e688a0ee84c8d87df
stanley :) <88297063+stanleyylin@users.noreply.github.com>
2025-11-18 21:40:28 -0500

    Time: 116 ms (59.81%) | Memory: 22 MB (67.35%) - LeetSync

--- 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.py
+++ 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.py
@@ -1,28 +1,35 @@
 class Solution:
     def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
-        A = []
-        start = 0
-        while True:
-            i = s.find(a, start)
-            if i == -1:
-                break
-            A.append(i)
-            start = i + 1
-
-        B = [] 
-        start = 0
-        while True:
-            i = s.find(b, start)
-            if i == -1:
-                break
-            B.append(i)
-            start = i + 1
+        def kmp(pattern, text):
+            lps = [0] * len(pattern)
+            j = 0
+            for i in range(1, len(pattern)):
+                while j > 0 and pattern[i] != pattern[j]:
+                    j = lps[j-1]
+                if pattern[i] == pattern[j]:
+                    j += 1
+                lps[i] = j
+            out = []
+            j = 0
+            for i in range(len(text)):
+                while j > 0 and text[i] != pattern[j]:
+                    j = lps[j-1]
+                if text[i] == pattern[j]:
+                    j += 1
+                if j == len(pattern):
+                    out.append(i - j + 1)
+                    j = lps[j - 1]
+            return out
+        A = kmp(a, s)
+        B = kmp(b, s)
         
         out = []
+        j = 0
         for i in A:
-            for j in B:
-                if abs(i-j) <= k:
-                    out.append(i)
-                    break
+            while j < len(B) and B[j] < i - k: # direction is important
+                j += 1
+            if j < len(B) and abs(B[j]-i) <= k:
+                out.append(i)
+
         return out
 
