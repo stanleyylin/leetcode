eeff8b1f3772a8e4d3e049dcebaa3f5236c3160c
stanley :) <88297063+stanleyylin@users.noreply.github.com>
2025-11-21 23:52:09 -0500

    Time: 631 ms (50.77%) | Memory: 57.2 MB (33.05%) - LeetSync

--- 1928-number-of-orders-in-the-backlog/number-of-orders-in-the-backlog.py
+++ 1928-number-of-orders-in-the-backlog/number-of-orders-in-the-backlog.py
@@ -6,41 +6,27 @@ class Solution:
         for price, amount, orderType in orders:
             if orderType == 0:
                 while amount > 0:
-                    if len(sellBL) == 0:
+                    if not sellBL or sellBL[0][0] > price:
                         heapq.heappush(buyBL, (-price, amount))
                         break
                     sellPrice, sellAmount = heapq.heappop(sellBL)
-                    if sellPrice <= price:
-                        use = min(amount, sellAmount)
-                        sellAmount -= use
-                        amount -= use
-                        if sellAmount > 0:
-                            heapq.heappush(sellBL, (sellPrice, sellAmount))
-                    else:
-                        heapq.heappush(buyBL, (-price, amount))
+                    use = min(amount, sellAmount)
+                    sellAmount -= use
+                    amount -= use
+                    if sellAmount > 0:
                         heapq.heappush(sellBL, (sellPrice, sellAmount))
-                        break
             else:
                 while amount > 0:
-                    if len(buyBL) == 0:
+                    if not buyBL or -buyBL[0][0] < price:
                         heapq.heappush(sellBL, (price, amount))
                         break
                     buyPrice, buyAmount = heapq.heappop(buyBL)
                     buyPrice = -buyPrice
-                    if buyPrice >= price:
-                        use = min(amount, buyAmount)
-                        buyAmount -= use
-                        amount -= use
-                        if buyAmount > 0:
-                            heapq.heappush(buyBL, (-buyPrice, buyAmount))
-                    else:
-                        heapq.heappush(sellBL, (price, amount))
+                    use = min(amount, buyAmount)
+                    buyAmount -= use
+                    amount -= use
+                    if buyAmount > 0:
                         heapq.heappush(buyBL, (-buyPrice, buyAmount))
-                        break
-        total = 0
         mod = 10**9 + 7
-        for _, amount in buyBL:
-            total += (amount % mod)
-        for _, amount in sellBL:
-            total += (amount % mod)
+        total = (sum(b for _, b in buyBL)%mod) + (sum(s for _, s in sellBL)%mod)
         return total % mod
