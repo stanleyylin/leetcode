9c6119a2d613062fc1fd8f7dffca751631bcf622
stanley :) <88297063+stanleyylin@users.noreply.github.com>
2025-11-24 00:17:08 -0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (30.80%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,15 +1,27 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        timestamps = []
-        for numPassengers, start, end in trips:
-            timestamps.append([start, numPassengers])
-            timestamps.append([end, -numPassengers])
+        timestamp = [0] * 1001
+        for num, start, end in trips:
+            timestamp[start] += num
+            timestamp[end] += -num
         
-        timestamps.sort()
-
         used_capacity = 0
-        for time, passenger_change in timestamps:
+        for passenger_change in timestamp:
             used_capacity += passenger_change
             if used_capacity > capacity:
                 return False
         return True
+
+        # timestamps = []
+        # for numPassengers, start, end in trips:
+        #     timestamps.append([start, numPassengers])
+        #     timestamps.append([end, -numPassengers])
+        
+        # timestamps.sort()
+
+        # used_capacity = 0
+        # for time, passenger_change in timestamps:
+        #     used_capacity += passenger_change
+        #     if used_capacity > capacity:
+        #         return False
+        # return True
